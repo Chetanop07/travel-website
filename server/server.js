aa245b42f69e53f87d829df2ac231fb282890147
chetan verma <fflegend2004@gmail.com>
2026-04-09 22:41:29 +0530

    final try

--- server/server.js
+++ server/server.js
@@ -31,7 +31,6 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.error("MongoDB connection error:", err));
-
 // =======================
 // MODELS
 // =======================
